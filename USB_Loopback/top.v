8a7b975d490e14691658975e88d2422a5f56c78c
tyzhao0931 <45839386+tyzhao0931@users.noreply.github.com>
2018-12-13 15:51:55 -0800

    Update top.v

--- USB_Loopback/top.v
+++ USB_Loopback/top.v
@@ -9,36 +9,27 @@ SB_HFOSC inthosc (
   .CLKHFPU(1'b1),
   .CLKHFEN(1'b1),
   .CLKHF(clk)
-);
+);                            //internal clock
 
-//reg          clk    ;
-//reg   [7:0]  data_i ;
-//wire         tx     ;
 wire         tx_done;
-wire         rx_done;
-//wire         rx     ;
-//wire  [7:0]  data_o ;
 wire  [7:0] datapool;
+wire  senden;
 wire  txtemp;
-    
-	//assign          rx = tx;// 
 	
     uart_rx u_uart_rx(
         .clk    (clk    ),
 	    .rx     (rx     ),
 	    .data_o (datapool),
-	    .rx_done(rx_done)
+	    .rx_done(senden)
     );
-
-    //assign data_i = data_o;
 
     uart_tx u_uart_tx(
         .clk    ( clk    ),
 	    .data_i ( datapool ),
+	    .send_en(senden),
 	    .tx     ( txtemp     ),
 	    .tx_done( tx_done)
     );	
 assign tx = txtemp;
     
 endmodule
-
